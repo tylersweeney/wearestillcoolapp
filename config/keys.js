700cb55c5c98d6b81aea7701438134a9138a389c
Tyler <tylersweeney@gmail.com>
2018-03-13 18:50:07 -0700

    add keys.js

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+//add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID:'524751588002-ar45htsd764aq7ganc1icr6u5rsi5d1c.apps.googleusercontent.com',
+        clientSecret:'PnAtxuwTImckLtrHD-gPQloJ'
+    },
+    mongodb: {
+        dbURI: 'mongodb://test:test@ds243768.mlab.com:43768/ty-oauth-test'
+    },
+    session: {
+        cookieKey:"roodoodoodadoo"
+    }
+};
