31d834e0a5b6b5b73949827f0ec6a7486881ed22
Alwyn Durham <alwyn.durham@evisions.com>
2018-03-31 15:56:33 -0700

    Trying to get css to work... no luck... pushing so i can share

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -3,7 +3,8 @@
  */
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 // define paths
 const nodeModulesPath = path.resolve(__dirname, '../node_modules');
@@ -30,31 +31,65 @@ module.exports = {
   },
 
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loaders: [ 'react-hot-loader', 'babel-loader' ],
+        use: [
+          // { loader: 'react-hot-loader/webpack' },
+          { loader: 'babel-loader' },
+        ],
         exclude: [nodeModulesPath],
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract(
-          'style-loader',
-          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader?sourceMap=inline'
-        ),
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          },
+        ],
       },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
       { test: /\.svg$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
     ],
+
+    // loaders: [
+    //   {
+    //     test: /\.js$/,
+    //     loaders: [ 'react-hot-loader', 'babel-loader' ],
+    //     exclude: [nodeModulesPath],
+    //   },
+    //   {
+    //     test: /\.css$/,
+    //     loader: ExtractTextPlugin.extract(
+    //       'style-loader',
+    //       'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader?sourceMap=inline'
+    //     ),
+    //   },
+    //   { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
+    //   { test: /\.svg$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+    // ],
+
   },
 
   postcss: [ require('autoprefixer'), require('postcss-nesting') ],
 
   plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new ExtractTextPlugin('style.css', { allChunks: true }),
+    // new webpack.optimize.DedupePlugin(),
+    // new webpack.optimize.OccurrenceOrderPlugin(),
+    // new webpack.optimize.UglifyJsPlugin(),
+
+    // new ExtractTextPlugin('style.css', { allChunks: true }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
   ],
 
   resolve: {
