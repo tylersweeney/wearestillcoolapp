c4256e47778f868ff9e0bc141df2134f3d9c6beb
Tyler <tylersweeney@gmail.com>
2018-11-14 20:50:15 -0800

    remove I can has cheeseburger ancient meme

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -35,11 +35,11 @@ passport.use(
         	username: profile.displayName,
 					googleId: profile.id,
 					thumbnail: profile._json.image.url,
-          gender: profile.gender || '',
-          email: profile.emails[0].value || '',
-          url: profile.url || '',
-          org: profile.organizations[0].name || '',
-          title: profile.organizations[0].title || ''
+        //   gender: profile.gender || '',
+        //   email: profile.emails[0].value || '',
+        //   url: profile.url || '',
+        //   org: profile.organizations[0].name || '',
+        //   title: profile.organizations[0].title || ''
     	}).save().then((newUser)=>{
 				console.log('new user created: ' + newUser);
 				done(null, newUser);
